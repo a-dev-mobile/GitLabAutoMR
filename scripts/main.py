07df78e86e109ac783f4a72af2f5057f5dd0ab87
Трофимов Дитрий Игоревич <trofimovdi@mts.ru>
2024-06-27 16:09:18 +0300

    wip

--- scripts/main.py
+++ scripts/main.py
@@ -5,6 +5,7 @@ import gitlab
 import os
 from prettytable import PrettyTable, ALL
 import tempfile
+import sys
 
 # Включаем текст сертификата прямо в исходный код
 CERTIFICATE = """<sertificate content>"""
@@ -34,10 +35,10 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="Скрипт для автоматизации создания веток и Merge Request в GitLab.")
     parser.add_argument("--proj_path", required=True, help="Путь к проекту.")
     parser.add_argument("--jira_task_id", required=True, help="ID задачи в Jira.")
-    parser.add_argument("--jira_task_name", required=True, help="Имя задачи в Jira.")
-    parser.add_argument("--initial_commit_msg", default="first commit", help="Сообщение первого коммита.")
-    parser.add_argument("--task_desc_en", required=True, help="Описание задачи на английском.")
-    parser.add_argument("--branch_kind", default="feature", help="Тип ветки (feature, hotfix, bugfix, release).")
+    parser.add_argument("--is_draft", action='store_true', help="Создать MR как draft.")
+    parser.add_argument("--mr_title", required=True, help="Заголовок Merge Request.")
+    parser.add_argument("--branch_desc", required=True, help="Дополнительный текст к названию ветки.")
+    parser.add_argument("--branch_type", default="feature", help="Тип ветки (feature, hotfix, bugfix, release).")
     parser.add_argument("--base_branch", default="dev", help="Целевая ветка.")
     parser.add_argument("--gl_proj_id", type=int, required=True, help="ID проекта в GitLab.")
     parser.add_argument("--gl_base_url", default="https://gitlab.com/", help="Базовый URL GitLab.")
@@ -49,20 +50,20 @@ def parse_arguments():
 
 def validate_arguments(args):
     """Проверяет аргументы командной строки на корректность"""
-    if not args.jira_task_id and not args.task_desc_en:
-        raise ValueError("И jira_task_id, и task_desc_en пустые.")
+    if not args.jira_task_id and not args.branch_desc:
+        raise ValueError("И jira_task_id, и branch_desc пустые.")
 
-    if args.task_desc_en and not re.match(r'^[a-z0-9_]+$', args.task_desc_en):
-        raise ValueError("task_desc_en должно содержать только латинские буквы в нижнем регистре, цифры и нижние подчеркивания.")
+    if args.branch_desc and not re.match(r'^[a-z0-9_]+$', args.branch_desc):
+        raise ValueError("branch_desc должно содержать только латинские буквы в нижнем регистре, цифры и нижние подчеркивания.")
 
 def generate_branch_name(args):
     """Генерирует имя новой ветки на основе аргументов"""
     if not args.jira_task_id:
-        return f"{args.branch_kind}/{args.task_desc_en}-short", f"[{args.branch_kind.capitalize()}] {args.jira_task_name}"
-    elif args.task_desc_en:
-        return f"{args.branch_kind}/{args.jira_task_id}_{args.task_desc_en}-short", f"[{args.branch_kind.capitalize()}][{args.jira_task_id}] {args.jira_task_name}"
+        return f"{args.branch_type}/{args.branch_desc}-short", args.mr_title
+    elif args.branch_desc:
+        return f"{args.branch_type}/{args.jira_task_id}_{args.branch_desc}-short", args.mr_title
     else:
-        return f"{args.branch_kind}/{args.jira_task_id}-short", f"[{args.branch_kind.capitalize()}][{args.jira_task_id}] {args.jira_task_name}"
+        return f"{args.branch_type}/{args.jira_task_id}-short", args.mr_title
 
 def branch_exists(repo, branch_name):
     """Проверяет существование ветки локально и на удаленных репозиториях"""
@@ -101,89 +102,100 @@ def mr_exists(project, branch_name):
     return len(mrs) > 0, mrs[0].web_url if mrs else None
 
 def main():
-    args = parse_arguments()
-    validate_arguments(args)
-    configure_environment(CERTIFICATE)
-    project_name = get_project_name(args.proj_path)
-    new_branch_name, mr_title = generate_branch_name(args)
+    try:
+        args = parse_arguments()
+        validate_arguments(args)
+        configure_environment(CERTIFICATE)
+        project_name = get_project_name(args.proj_path)
+        new_branch_name, mr_title = generate_branch_name(args)
 
-    repo = Repo(args.proj_path)
-    current_branch_name = repo.active_branch.name
+        repo = Repo(args.proj_path)
+        current_branch_name = repo.active_branch.name
 
-    if repo.is_dirty(untracked_files=True):
-        user_response = input(f"Целевая ветка '{args.base_branch}' содержит изменения. Продолжить? (y/n): ")
-        if user_response.lower() != 'y':
-            raise RuntimeError("Процесс прерван пользователем.")
-    
-    if current_branch_name != args.base_branch:
-        raise RuntimeError(f"Вы не на ветке '{args.base_branch}'. Вы на ветке '{current_branch_name}'. Необходимо переключиться на целевую ветку '{args.base_branch}' для продолжения.")
+        if repo.is_dirty(untracked_files=True):
+            user_response = input(f"Целевая ветка '{args.base_branch}' содержит изменения. Продолжить? (y/n): ")
+            if user_response.lower() != 'y':
+                raise RuntimeError("Процесс прерван пользователем.")
 
-    gl = gitlab.Gitlab(args.gl_base_url, private_token=args.gl_token)
-    project = gl.projects.get(args.gl_proj_id)
+        if current_branch_name != args.base_branch:
+            raise RuntimeError(f"Вы не на ветке '{args.base_branch}'. Вы на ветке '{current_branch_name}'. Необходимо переключиться на целевую ветку '{args.base_branch}' для продолжения.")
 
-    location = branch_exists(repo, new_branch_name)
-    if location:
-        mr_exist, mr_url = mr_exists(project, new_branch_name)
-        if mr_exist:
-            raise RuntimeError(f"Merge Request для ветки '{new_branch_name}' уже существует. URL: {mr_url}")
+        gl = gitlab.Gitlab(args.gl_base_url, private_token=args.gl_token)
+        project = gl.projects.get(args.gl_proj_id)
+
+        location = branch_exists(repo, new_branch_name)
+        if location:
+            mr_exist, mr_url = mr_exists(project, new_branch_name)
+            if mr_exist:
+                raise RuntimeError(f"Merge Request для ветки '{new_branch_name}' уже существует. URL: {mr_url}")
+            else:
+                raise RuntimeError(f"Ветка {new_branch_name} уже существует {location}.")
+
+
+        print("\n--- Подтверждение создания ветки и отправки на удаленный репозиторий ---\n")
+        branch_info = [
+            ("Project", project_name),
+            ("Branch", new_branch_name),
+            ("Author", args.author_name),
+            ("Email", args.author_email),
+        ]
+        print_table(branch_info)
+
+        mr_desc_path = os.path.join(args.proj_path, args.mr_desc_file)
+        mr_description = None
+        if os.path.exists(mr_desc_path):
+            with open(mr_desc_path, 'r', encoding='utf-8') as file:
+                mr_description = file.read()
         else:
-            raise RuntimeError(f"Ветка {new_branch_name} уже существует {location}.")
+            print(f"Файл для описания Merge Request по пути '{args.mr_desc_file}' не найден.")
 
-    print("\n--- Подтверждение создания ветки и отправки на удаленный репозиторий ---\n")
-    branch_info = [
-        ("Project", project_name),
-        ("Branch", new_branch_name),
-        ("Author", args.author_name),
-        ("Email", args.author_email),
-    ]
-    print_table(branch_info)
-    
-    confirm_branch = input("Вы согласны создать и отправить ветку с такими данными? (y/n): ")
-    if confirm_branch.lower() != "y":
-        raise RuntimeError("Создание ветки и отправка  - отменены пользователем.")
+        print("\n--- Предварительный просмотр данных MR ---\n")
+        
+        mr_title_display = f"{'Draft: ' if args.is_draft else ''}{mr_title}"
+        mr_info = [
+            ("Project", project_name),
+            ("Title", mr_title_display),
+            ("Target branch", args.base_branch),
+            ("Description", mr_description if mr_description else "Описание отсутствует."),
+        ]
+        print_table(mr_info)
 
-    new_branch_ref = repo.create_head(new_branch_name)
-    new_branch_ref.checkout()
-    repo.remote().push(refspec=f"{new_branch_name}:{new_branch_name}")
-    print(f"✔ Ветка '{new_branch_name}' успешно создана и отправлена в удаленный репозиторий.")
+        confirm_branch = input("Вы согласны создать и отправить ветку с такими данными? (y/n): ")
+        if confirm_branch.lower() != "y":
+            raise RuntimeError("Создание ветки и отправка - отменены пользователем.")
 
-    mr_desc_path = os.path.join(args.proj_path, args.mr_desc_file)
-    mr_description = None
-    if os.path.exists(mr_desc_path):
-        with open(mr_desc_path, 'r', encoding='utf-8') as file:
-            mr_description = file.read()
-    else:
-        print(f"Файл для описания Merge Request по пути '{args.mr_desc_file}' не найден.")
+        new_branch_ref = repo.create_head(new_branch_name)
+        new_branch_ref.checkout()
+        repo.remote().push(refspec=f"{new_branch_name}:{new_branch_name}")
+        print(f"✔ Ветка '{new_branch_name}' успешно создана и отправлена в удаленный репозиторий.")
 
-    
-    print("\n--- Подтверждение создания Merge Request ---\n")
-    mr_info = [
-        ("Project", project_name),
-        ("Title", mr_title),
-        ("Target branch", args.base_branch),
-        ("Description", mr_description if mr_description else "Описание отсутствует."),
-    ]
-    print_table(mr_info)
-    confirm_mr = input("Согласны ли вы отправить Merge Request с такими данными? (y/n): ")
-    if confirm_mr.lower() != "y":
-        raise RuntimeError("Merge Request отменен пользователем.")
-    mr_data = {
-        "source_branch": new_branch_name,
-        "target_branch": args.base_branch,
-        "title": mr_title,
-        "author": {
-            "name": args.author_name,
-            "email": args.author_email
-        },
-        "squash": True,
-    }
-    if mr_description:
-        mr_data["description"] = mr_description
-    mr = project.mergerequests.create(mr_data)
-    print("\n✔ Merge Request создан")
-    print("")
-    print(mr.title)
-    print(mr.web_url)
+        print("\n--- Подтверждение создания Merge Request ---\n")
+
+        confirm_mr = input("Согласны ли вы отправить Merge Request с такими данными? (y/n): ")
+        if confirm_mr.lower() != "y":
+            raise RuntimeError("Merge Request отменен пользователем.")
+        mr_data = {
+            "source_branch": new_branch_name,
+            "target_branch": args.base_branch,
+            "title": mr_title_display,
+            "author": {
+                "name": args.author_name,
+                "email": args.author_email
+            },
+            "squash": True,
+        }
+        if mr_description:
+            mr_data["description"] = mr_description
+        mr = project.mergerequests.create(mr_data)
+        print("\n✔ Merge Request создан")
+        print("")
+        print(mr.title)
+        print(mr.web_url)
+        
+    except Exception as e:
+        print(f"Произошла ошибка: {str(e)}", file=sys.stderr)
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
